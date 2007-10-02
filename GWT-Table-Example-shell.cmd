a76535468c94a162fc7c146708c0d239aaa53cdf
svetlin.nakov <svetlin.nakov@96c85b68-1e38-0410-bd9a-b1cab423ab52>
2007-10-02 15:04:07 +0000

    Modified the shell scripts to support GWT 1.4.60

--- GWT-Table-Example-shell.cmd
+++ GWT-Table-Example-shell.cmd
@@ -1,2 +1,2 @@
-SET GWT_HOME=C:\Progra~1\GWT-1.4.10
+SET GWT_HOME=C:\Progra~1\gwt-windows-1.4.60
 java -cp src;bin;%GWT_HOME%/gwt-user.jar;%GWT_HOME%/gwt-dev-windows.jar com.google.gwt.dev.GWTShell -out www example.GWTAdvancedTableExample/GWTAdvancedTableExample.html
